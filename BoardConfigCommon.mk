8581d8e314332dccd2499ed5da7c0461e2df41ef
Shaun <shaunsibu@gmail.com>
2015-08-18 13:36:42 -0600

    Keep userdebug build variant

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,7 +150,7 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user userdebug),true)
     ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
       WITH_DEXPREOPT_BOOT_IMG_ONLY := true
     endif
